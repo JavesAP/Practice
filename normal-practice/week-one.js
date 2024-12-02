6dfd4aef228e08d8369693ad4c92fed93f59e62a
Javes Hernandez <javesap3@gmail.com>
2024-12-02 16:02:39 -0500

    Create practice folder and add first problem

---
+++ normal-practice/week-one.js
@@ -0,0 +1,8 @@
+// 12-2-2024
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+// Implement a function that determines whether a string containing letters is an isogram. 
+// Assume the empty string is an isogram. Ignore letter case.
+
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
