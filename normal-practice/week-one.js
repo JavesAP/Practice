c9a8d3e393e4f4705e112430b009020c1dcc64cd
Javes Hernandez <javesap3@gmail.com>
2024-12-02 21:57:15 -0500

    Solve week one day one problem

--- normal-practice/week-one.js
+++ normal-practice/week-one.js
@@ -6,3 +6,19 @@
 // "Dermatoglyphics" --> true
 // "aba" --> false
 // "moOse" --> false (ignore letter case)
+
+function isIsogram(str){
+  const stringLetters = str.toLowerCase().split('')
+  const checkingArr = []
+  let boolean = true
+    
+  stringLetters.forEach((letter) => {
+    if (!checkingArr.includes(letter)) checkingArr.push(letter)
+    else boolean = false
+  })
+    
+  return boolean
+}
+
+// console.log(isIsogram('David')) --> false
+// console.log(isIsogram('Olympic')) --> true
