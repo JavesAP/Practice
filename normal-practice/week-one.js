d5afc00e3e91610e3ed6b895c885e70e2c33b1a5
Javes Hernandez <javesap3@gmail.com>
2024-12-03 21:19:43 -0500

    Add and solve second day problem

--- normal-practice/week-one.js
+++ normal-practice/week-one.js
@@ -22,3 +22,25 @@ function isIsogram(str){
 
 // console.log(isIsogram('David')) --> false
 // console.log(isIsogram('Olympic')) --> true
+
+
+// 12-2-2024
+// Write a function that checks if a given string is a palindrome 
+// meaning it reads the same forwards and backwards. 
+// Ensure the function ignores spaces, punctuation, and 
+// case differences when checking for a palindrome
+
+function isPalindrome (word) {
+  const cleanedWord = word.toLowerCase().replace(/[^a-z0-9]/g, '').split('')
+  let bool = true
+  
+  cleanedWord.forEach((letter, index, array) => {
+    const endLetter = array[array.length - (index + 1)]
+    if (letter != endLetter) bool = false
+  })
+  
+  return bool
+}
+
+//console.log('Racecar') --> true
+//console.log('steven') --> false
